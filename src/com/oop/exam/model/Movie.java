e0206d8209264ed97309bc9e68f7e60ae865e964
Ivan Uresti <ivan.uresti@gmail.com>
2021-03-09 08:37:27 -0600

    solucion

---
+++ src/com/oop/exam/model/Movie.java
@@ -0,0 +1,82 @@
+package com.oop.exam.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private int year;
+    private List<String> actors;
+    private String synopsis;
+    private String director;
+    private List<Platform> platforms;
+    private List<Review> reviews;
+
+    public Movie(String title, int year, String director) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+
+        reviews = new LinkedList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<String> actors) {
+        this.actors = actors;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public List<Platform> getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(List<Platform> platforms) {
+        this.platforms = platforms;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void addReview(Review review) {
+        reviews.add(review);
+    }
+}
--- src/com/oop/exam/service/MovieManager.java
+++ src/com/oop/exam/service/MovieManager.java
@@ -2,6 +2,7 @@ package com.oop.exam.service;
 
 import com.oop.exam.model.Movie;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class MovieManager {
@@ -13,5 +14,32 @@ public class MovieManager {
 
     public LinkedList<Movie> getMovies() {
         return movies;
+    }
+
+    public void addMovie(Movie lordOfTheRings1) {
+        movies.add(lordOfTheRings1);
+    }
+
+    public Movie findMovie(String title) {
+
+        for(Movie movie: movies){
+            if(movie.getTitle().equals(title)){
+                return movie;
+            }
+        }
+
+        /*Iterator<Movie> movieIterator = movies.iterator();
+
+        while(movieIterator.hasNext()){
+            Movie movie = movieIterator.next();
+
+            if(movie.getTitle().equals(title)){
+                return movie;
+            }
+        }*/
+
+        return null;
+
+        //return movies.stream().filter(movie -> movie.getTitle().equals(title)).findFirst().orElseThrow();
     }
 }
